6bc2c8da9f399a71e68cfeac1a54f8ad6be2c5fb
Kitman <kitman.yiu@zerologix.com>
2020-12-22 18:02:34 +1100

    trying to added enum to optimze the code

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -11,13 +11,20 @@ interface IHomeProps extends RouteComponentProps<{ title: string }> {
 
 }
 
+enum DataState {
+  LOADING,
+  ERROR,
+  NORMAL
+}
+
 interface IHomeState {
   isLoaded: boolean,
   cityData: [],
   weatherData: [],
   error: boolean,
   cacheData: any,
-  errorMessage: string
+  errorMessage: string,
+  dataState: DataState
 }
 
 class HomePage extends React.Component<IHomeProps, IHomeState> {
@@ -32,13 +39,26 @@ class HomePage extends React.Component<IHomeProps, IHomeState> {
       weatherData: [],
       error: false,
       cacheData: [],
-      errorMessage: ''
+      errorMessage: '',
+      dataState: DataState.LOADING
     };
     this.handleSearchDebounce = debounce(this.handleSearchPress, 300);
   }
 
-  // eslint-disable-next-line react/sort-comp
+  componentDidMount() {
+    this.getLocation()
+  }
+
+  setErrorMessage = (message: string): void => {
+    this.setState({error: true, isLoaded: true, errorMessage: message});
+  }
+
+  setLoading = (): void => {
+    this.setState({error: false, isLoaded: false, errorMessage: ''});
+  }
+
   handleSearchPress = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setLoading();
     try {
       const {cacheData} = this.state;
       const q = e.target.value.toLowerCase();
@@ -49,28 +69,31 @@ class HomePage extends React.Component<IHomeProps, IHomeState> {
         response = await getCities(q);
         cacheData[q] = response;
       }
-      this.setState({cityData: response.data.data, error: false, cacheData});
+      this.setState({cityData: response.data.data, cacheData});
     } catch {
-      this.setState({error: true, isLoaded: true, errorMessage: 'Server Error'});
+      this.setErrorMessage('Server Error');
     }
   };
 
   // eslint-disable-next-line no-undef
   getCityWeather = async (position: GeolocationPosition) => {
-    const result = await getCurrentLocation(position);
-    const response = await getWeather(result.data.data[0].woeid);
-    this.setState({
-        cityData: response.data.data[0],
-        error: false,
-        weatherData: response.data.data,
-        isLoaded: true
-      }
-    );
+    try {
+      const result = await getCurrentLocation(position);
+      const response = await getWeather(result.data.data[0].woeid);
+      this.setState({
+          cityData: response.data.data[0],
+          dataState: DataState.NORMAL,
+          weatherData: response.data.data,
+          isLoaded: true
+        }
+      );
+    } catch {
+      this.setErrorMessage('Server Error');
+    }
   }
 
   // eslint-disable-next-line no-undef
   getCityError = async (error: GeolocationPositionError) => {
-
     let messsage: string = '';
     switch (error.code) {
       case error.PERMISSION_DENIED:
@@ -83,31 +106,27 @@ class HomePage extends React.Component<IHomeProps, IHomeState> {
         messsage = "The request to get user location timed out."
         break;
       default:
-        messsage = "An unknown error occurred."
+        messsage = "Sever Error"
         break;
     }
-
-    this.setState({error: true, isLoaded: true, errorMessage: messsage});
+    this.setErrorMessage(messsage);
   };
 
   getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.getCityWeather, this.getCityError);
+    } else {
+      this.setErrorMessage("GEO Location API not available");
     }
   }
 
-  async componentDidMount() {
-    this.getLocation()
-  }
-
   selectCountry = async (cityID: number) => {
-    this.setState({isLoaded: false})
-    let result;
+    this.setLoading();
     try {
-      result = await getWeather(cityID);
+      const result = await getWeather(cityID);
       this.setState({weatherData: result.data.data, isLoaded: true})
     } catch {
-      this.setState({error: true, isLoaded: true, errorMessage: 'Server Error'});
+      this.setErrorMessage('Server Error');
     }
   }
 
@@ -117,6 +136,12 @@ class HomePage extends React.Component<IHomeProps, IHomeState> {
       <Cards data={weatherData} />
     );
 
+    const s = {
+      0: <Cards data={weatherData} />,
+      1: <p className="error">{errorMessage}</p>,
+      2: <div className="loading--fixed"><Loader /></div>,
+    };
+
     return (
       <div className="weather">
         <Header
@@ -124,10 +149,9 @@ class HomePage extends React.Component<IHomeProps, IHomeState> {
           selectCountry={this.selectCountry}
           data={cityData}
         />
-        {!isLoaded ? <div className="loading--fixed"><Loader /></div> : showCard}
+        {!isLoaded ? <div className="loading--fixed"><Loader/></div> : showCard}
       </div>
     );
-
   }
 }
 
